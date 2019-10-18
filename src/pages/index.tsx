fc88a7b7da9f35c104dec1d53e2ac9204956e67d
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-18 12:35:51 +0200

    feat: added sentry

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import * as Sentry from '@sentry/browser'
 
 import StyleBaseline from '../design-tokens/StyleBaseline'
 import StylesProvider from '../design-tokens/StylesProvider'
 
 import Layout from '../components/Layout'
+
+Sentry.init({ dsn: 'https://7d6236da009e4951a98980581bc55e78@sentry.io/1783802' })
 
 export default () => (
   <StylesProvider>
