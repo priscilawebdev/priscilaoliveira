86bd593b82db2119bd82eeb5f1c0b8ddf491c168
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-18 12:45:15 +0200

    refactor: replaced @sentry/browser by sentry gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://7d6236da009e4951a98980581bc55e78@sentry.io/1783802',
+        environment: process.env.NODE_ENV,
+        // enabled: (() => ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
+      },
+    },
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
