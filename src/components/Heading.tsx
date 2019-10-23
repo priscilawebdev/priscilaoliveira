8eed31cb609cc318be6f2d925256a4f774992514
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-23 15:33:09 +0200

    fix: removed console.log

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,9 +3,6 @@ import styled from 'styled-components'
 import { default as MaterialUITypography, TypographyProps } from '@material-ui/core/Typography'
 
 import { filterProps } from '../design-tokens/utils'
-import { SpacingProps, spacing } from '@material-ui/system'
-
-console.log('spacing', spacing)
 
 type HeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 type HeadingRef = HeadingType
@@ -54,7 +51,6 @@ const Heading = forwardRef<HeadingRef, Props>(function Heading(
   { variant = 'h6', fontWeight = 'normal', size = 'default', ...props },
   ref,
 ) {
-  console.log('size', sizeVariants[size])
   return <StyledMaterialUITypography {...props} variant={variant} fontWeight={fontWeight} size={size} ref={ref} />
 })
 
